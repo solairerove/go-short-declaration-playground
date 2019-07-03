c8c5e330b9325273e060285131767e7cae8806b7
Mikita Kryvitski <solairerove@gmail.com>
2019-07-03 20:45:00 +0300

    feature: short declaration playground

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	fmt.Println(x)
+
+	x = 99
+	fmt.Println(x)
+
+	y := 100 + 10
+	fmt.Println(y)
+
+	z := "Vse idet po planu"
+	fmt.Println(z)
+}
